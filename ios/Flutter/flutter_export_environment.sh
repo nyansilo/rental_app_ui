0dcad942a8120c80150a938f1136a4b096613cea
Daniel Geuza <danielgeuza808@gmail.com>
2020-02-10 11:21:02 +0800

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/pro/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/pro/Documents/FlutterChina/rental_ui"
+export "FLUTTER_TARGET=/Users/pro/Documents/FlutterChina/rental_ui/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/pro/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
